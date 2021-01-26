7d6e883eac3088aaddba2f0d05fc70070b05f3c6
Joshua Galit <joshuaimalay@gmail.com>
2021-01-26 19:59:49 +0800

    import TheNavBar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="flex flex-col bg-darkest min-h-screen h-screen text-gray-50 antialiased">
-    <Nuxt />
+    <TheNavBar />
+    <main>
+      <Nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      TheNavBar: () => import('~/components/TheNavBar')
+    }
+  }
+</script>
