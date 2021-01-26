16b1476c3e4fe82e6ea2a5bc820c36a62d086e3f
Joshua Galit <joshuaimalay@gmail.com>
2021-01-26 21:55:09 +0800

    extend color gray dark & darker

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: '#9147ff',
+        'gray-dark': '#464648',
+        'gray-darker': '#303032',
         'dark': '#1f1f23',
         'darker': '#18181b',
         'darkest': '#0e0e10'
